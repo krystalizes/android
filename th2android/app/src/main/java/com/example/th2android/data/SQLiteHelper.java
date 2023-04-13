d22e253ddcc74ba49e63d299380c266b5b7b0d54
krystalizes <91723136+krystalizes@users.noreply.github.com>
2023-04-13 12:05:16 +0700

    th2

---
+++ th2android/app/src/main/java/com/example/th2android/data/SQLiteHelper.java
@@ -0,0 +1,159 @@
+package com.example.th2android.data;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import com.example.th2android.model.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SQLiteHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "ChiTieu.db";
+    private static int DATABASE_VERSION = 1;
+    public SQLiteHelper(@Nullable Context context) {
+        super(context,DATABASE_NAME,null,DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE items("+
+                "id INTEGER PRIMARY KEY AUTOINCREMENT,"+
+                "title TEXT,"+
+                "category TEXT,"+
+                "price TEXT,"+
+                "date TEXT)";
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
+    @Override
+    public void onOpen(SQLiteDatabase db) {
+        super.onOpen(db);
+    }
+    public List<Item> getAll() {
+        List<Item> list = new ArrayList<>();
+        SQLiteDatabase st = getReadableDatabase();
+        String order = "date DESC";
+        Cursor rs = st.query("items",
+                null, null, null,
+                null, null, order);
+        while ((rs != null) && (rs.moveToNext())) {
+            int id= rs.getInt(0);
+            String t = rs.getString(1);
+            String c = rs.getString(2);
+            String p = rs.getString(3);
+            String d = rs.getString(4);
+            list.add(new Item(id,t,c,p,d));
+        }
+        return list;
+    }
+    public long addItem(Item i){
+        ContentValues values = new ContentValues();
+        values.put("title", i.getTitle());
+        values.put("category", i.getCategory());
+        values.put("price", i.getPrice());
+        values.put("date", i.getDate());
+        SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+        return sqLiteDatabase.insert("items",null, values);
+    }
+    public List<Item> getByDate(String date) {
+        List<Item> list = new ArrayList<>();
+        String whereClause = "date like ?";
+        String[] whereArgs = {date};
+        SQLiteDatabase sqLiteDatabase = getReadableDatabase();
+        Cursor rs = sqLiteDatabase.query("items",
+                null, whereClause, whereArgs,
+                null, null, null);
+        while (rs != null && rs.moveToNext()) {
+            int id= rs.getInt(0);
+            String t = rs.getString(1);
+            String c = rs.getString(2);
+            String p = rs.getString(3);
+            list.add(new Item(id,t,c,p,date));
+        }
+        return list;
+    }
+    public int updateItem(Item i) {
+        ContentValues values = new ContentValues();
+        values.put("title", i.getTitle());
+        values.put("category", i.getCategory());
+        values.put("price", i.getPrice());
+        values.put("date", i.getDate());
+        SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+        String whereClause = "id = ?";
+        String[] whereArgs = {Integer.toString(i.getId())};
+        return sqLiteDatabase.update("items",
+                values, whereClause, whereArgs);
+    }
+    public int deleteItem(int id) {
+        String whereClause = "id = ?";
+        String[] whereArgs = {Integer.toString(id)};
+        SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+        return sqLiteDatabase.delete("items",
+                whereClause, whereArgs);
+    }
+    public List<Item> searchByTitle(String key) {
+        List<Item> list= new ArrayList<>();
+        String whereClause = "title like ?";
+        String[] whereArgs = {"%"+key+"%"};
+        SQLiteDatabase sqLiteDatabase = getReadableDatabase();
+        Cursor rs = sqLiteDatabase.query("items",
+                null, whereClause, whereArgs,
+                null, null, null);
+        while ((rs != null) && (rs.moveToNext())) {
+            int id= rs.getInt(0);
+            String t = rs.getString(1);
+            String c = rs.getString(2);
+            String p = rs.getString(3);
+            String d = rs.getString(4);
+            list.add(new Item(id,t,c,p,d));
+        }
+        return list;
+    }
+    public List<Item> searchByCategory(String category) {
+        List<Item> list= new ArrayList<>();
+        String whereClause = "category like ?";
+        String[] whereArgs = {category};
+        SQLiteDatabase sqLiteDatabase = getReadableDatabase();
+        Cursor rs = sqLiteDatabase.query("items",
+                null, whereClause, whereArgs,
+                null, null, null);
+        while ((rs != null) && (rs.moveToNext())) {
+            int id= rs.getInt(0);
+            String t = rs.getString(1);
+            String c = rs.getString(2);
+            String p = rs.getString(3);
+            String d = rs.getString(4);
+            list.add(new Item(id,t,c,p,d));
+        }
+        return list;
+    }
+    public List<Item> searchByDateFromTo(String from,String to) {
+        List<Item> list = new ArrayList<>();
+        String whereClause = "date BETWEEN ? AND ?";
+        String[] whereArgs = { from.trim(),to.trim()};
+        SQLiteDatabase sqLiteDatabase = getReadableDatabase();
+        Cursor rs = sqLiteDatabase.query("items",
+                null, whereClause, whereArgs,
+                null, null, null);
+        while ((rs != null) && (rs.moveToNext())) {
+            int id= rs.getInt(0);
+            String t = rs.getString(1);
+            String c = rs.getString(2);
+            String p = rs.getString(3);
+            String d = rs.getString(4);
+            list.add(new Item(id,t,c,p,d));
+        }
+        return list;
+    }
+}
---
+++ th2android/app/src/main/java/com/example/th2android/fragment/FragmentHome.java
@@ -0,0 +1,83 @@
+package com.example.th2android.fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import com.example.th2android.R;
+import com.example.th2android.UpdateDeleteActivity;
+import com.example.th2android.adapter.RecycleViewAdapter;
+import com.example.th2android.data.SQLiteHelper;
+import com.example.th2android.model.Item;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class FragmentHome extends Fragment implements RecycleViewAdapter.ItemListener{
+    private RecyclerView recyclerView;
+    RecycleViewAdapter adapter;
+    private SQLiteHelper db;
+    private TextView tvTong;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        recyclerView=view.findViewById(R.id.recycleView);
+        tvTong=view.findViewById(R.id.tvTong);
+        adapter=new RecycleViewAdapter();
+        db=new SQLiteHelper(getContext());
+        Date d=new Date();
+        SimpleDateFormat f=new SimpleDateFormat("d/MM/yyyy");
+        List<Item> list=db.getByDate(f.format(d));
+        Log.d("TAG", "List of items: " + list.toString());
+        adapter.setList(list);
+        tvTong.setText("Tong tien:"+tong(list));
+        LinearLayoutManager manager=new LinearLayoutManager(getContext(),RecyclerView.VERTICAL,false);
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setAdapter(adapter);
+        adapter.setItemListener(this);
+    }
+    private int tong(List<Item> list){
+        int t=0;
+        for(Item i:list){
+            t+=Integer.parseInt(i.getPrice());
+        }
+        return t;
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Item item= adapter.getItem(position);
+        Intent intent=new Intent(getActivity(), UpdateDeleteActivity.class);
+        intent.putExtra("item",item);
+        startActivity(intent);
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        Date d=new Date();
+        SimpleDateFormat f=new SimpleDateFormat("d/MM/yyyy");
+        List<Item> list=db.getByDate(f.format(d));
+        adapter.setList(list);
+        tvTong.setText("Tong tien:"+tong(list));
+    }
+}
